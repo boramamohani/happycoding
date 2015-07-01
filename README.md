04ff4386d18b3b515c3ec0e04076c005cf462137
keepcosmos <keepcosmos@gmail.com>
2015-07-01 10:56:38 +0900

    add initial files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # happycoding
+
+도시 리스트(프랑스): https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/country.json
+상품정보: https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/offers.json
+가이드정보: https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/guides.json
+통화 정보: https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/currencies.json
---
+++ common.js
@@ -0,0 +1,4 @@
+var countryJsonUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/country.json';
+var offersUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/offers.json';
+var guidesUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/guides.json';
+var currenciesUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/currencies.json';
