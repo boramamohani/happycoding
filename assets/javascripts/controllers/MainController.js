aee995e02a83d8130ed3798731cabc995eab8c64
boramamohani <boramhan@gmail.com>
2015-07-08 23:37:53 +0900

    showing guide info

--- assets/javascripts/controllers/MainController.js
+++ assets/javascripts/controllers/MainController.js
@@ -6,43 +6,43 @@ var currenciesUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	
-	(function getOffers() {
-		$scope.offers = {} ; 
+	(function getTours() {
+		$scope.tours = {} ; 
 
 		$http({method: 'GET', url: offersUrl})
-		.success(function(data, status) {
-			$scope.offers.status = status ; 
-			$scope.offers.data = data ;
+			.success(function(data, status) {
+				console.log(status) ; 
 
-			alert($scope.offers.data[2].title) ; 
-		})
-		.error(function(data, status) {
-			$scope.offers.status = status ; 
-			$scope.offers.data = data || "request failed" ; 
+				$scope.tours.status = status ; 
+				$scope.tours.data = data ;
 
-			console.error("ERROR: failed at getting Offers data") ; 
-		}) ; 	
+				// alert($scope.offers.data[2].title) ; 
+
+				$http({method: 'GET', url: guidesUrl})
+					.success(function(guidesData, guidesStatus) {
+						$scope.tours.status = guidesStatus ; 
+						var guides = guidesData ; 
+
+						_.each($scope.tours.data, function searchOurGuide(element, index, list) {
+							console.log(element.guide_id) ; 
+							console.log(guides) ; 
+							var ourGuide = _.find(guides, function(guide) {
+								return guide.id ===	element.guide_id ; 
+							}) ;
+							$scope.tours.data[index].username = ourGuide.user.username ; 
+							$scope.tours.data[index].profile_medium_url = ourGuide.user.profile_medium_url ;  
+						});
+					})
+					.error(function(data, status) {
+						console.error("ERROR: failed at getting Guides data") ;
+					}) ; 
+			})
+			.error(function(data, status) {
+				$scope.offers.status = status ; 
+				$scope.offers.data = data || "request failed" ; 
+
+				console.error("ERROR: failed at getting Tours data") ; 
+			}) ; 	
 	}) () ; 
-
-	(function getGuides() {
-		$scope.guides = {} ; 
-
-		$http({method: 'GET', url: guidesUrl})
-		.success(function(data, status) {
-			$scope.guides.status = status ; 
-			$scope.guides.data = data ;
-
-			alert($scope.guides.data[4].user.username) ; 
-		})
-		.error(function(data, status) {
-			$scope.guides.status = status ; 
-			$scope.guides.data = data || "request failed" ; 
-
-			console.error("ERROR: failed at getting Guides data") ; 
-		}) ; 	
-	}) () ; 
-
-	
-
 
 }]) ; 
