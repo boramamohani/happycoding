cb44f239d8ecba8aacb9e85a934e8656e7b07e8f
boramamohani <boramhan@gmail.com>
2015-08-01 03:01:21 +0900

    commit again

--- assets/javascripts/MainController.js
+++ assets/javascripts/MainController.js
@@ -1,123 +1,100 @@
-var LEFT_SIDE_CURRENCIES = ["KRW", "USD", "EUR", "JPY", "TWD", "THB", "NZD", "MYR", "HKD", "GBP", "CNY", "CAD", "AUD" ] ; 
-var RIGHT_SIDE_CURRENCIES = ["CHF", "RUB", "DKK"] ; 
+angular.module('mainController', [])
+	.controller('mainCtrl', ['$scope', 'processTourData', function($scope, processTourData) {		
+		processTourData(function(tours) {
+			$scope.tours = tours ; 
+		}) ; 
+
+		$scope.itemsPerPage = 12 ; 
+		$scope.currentPage = 1 ; 
+
+		$scope.scoreToStar = function(tour) {
+			if (tour.score > 4.0) return 'fiveStar' ; 
+			else if (tour.score <= 4.0 && tour.score >3.0) return 'fourStar' ;
+			else if (tour.score <= 3.0 && tour.score > 2.0) return 'threeStar' ; 
+			else if (tour.score <= 2.0 && tour.score > 1.0) return 'twoStar' ; 
+			else if (tour.score <= 1.0 && tour.score > 0) return 'oneStar' ; 
+			else if (tour.score === 0) return 'zeroStar' ;
+			else return "" ; 
+		} ; 
+
+		$scope.pageRange = function() {
+			var rangeSize = 5 ; 
+			var leftOffset = Math.floor(rangeSize/2); 
+			var rightOffset = $scope.totalPages() - leftOffset ; 
+			//alert($scope.totalPages() ) ; 
+
+			var ret = [] ; 
+			var start = $scope.currentPage ; 
+
+			if (start <= leftOffset) {
+				start = 1 ;
+			} else if (start >= rightOffset) {
+				start = $scope.totalPages() - rangeSize + 1 ; 
+			} else {
+				start = start - leftOffset
+			}			
+			var end = ((start + rangeSize) > $scope.totalPages()) ? $scope.totalPages() : start + rangeSize -1;  
+			for (var i = start ; i <= end  ; i++) { ret.push(i) ; } 
+			return ret ; 
+		} ;	
+
+		$scope.totalPages = function() {
+			return Math.ceil($scope.tours.length / $scope.itemsPerPage) ; 
+		} ;
+
+		$scope.setPage = function(n) {
+			$scope.currentPage = n ; 
+		} ;
+
+		$scope.prevPage = function() {
+			if ($scope.currentPage > 1) {$scope.currentPage--} ;
+		} ; 
+
+		$scope.prevPageDisabled = function() {
+			return $scope.currentPage === 1 ? "disabled" : "" ;
+		} ;
+
+		$scope.nextPage = function() {
+			if ($scope.currentPage < $scope.totalPages()) {$scope.currentPage++ };
+		} ;
+
+		$scope.nextPageDisabled = function(){
+			return $scope.currentPage === $scope.totalPages() ? "disabled" : "" ; 
+		} ;
 
 
-angular.module('mainController', []).controller('mainCtrl', ['$scope', 'processData', function($scope, processData) {		
-	processData(function(tours) {
-		console.log(tours) ;  
-		$scope.tours = tours ; 
-	}) ; 
+		$scope.typeFilter = {
+			individual: true, 
+			group: true
+		} ; 
 
-	$scope.itemsPerPage = 12 ; 
-	$scope.currentPage = 1 ; 
+		$scope.durationFilter = {
+			halfDay: true,
+			day: true,
+			dayPlus: true,
+			flexible: true
+		} ; 
 
-	$scope.scoreToStar = function(tour) {
-		if (tour.score > 4.0) return 'fiveStar' ; 
-		else if (tour.score <= 4.0 && tour.score >3.0) return 'fourStar' ;
-		else if (tour.score <= 3.0 && tour.score > 2.0) return 'threeStar' ; 
-		else if (tour.score <= 2.0 && tour.score > 1.0) return 'twoStar' ; 
-		else if (tour.score <= 1.0 && tour.score > 0) return 'oneStar' ; 
-		else if (tour.score === 0) return 'zeroStar' ;
-		else return "" ; 
-	} ; 
+		$scope.transportationFilter = {
+			walk: true,
+			auto: true,
+			bike: true
+		} ;
 
-	$scope.pageRange = function() {
-		var rangeSize = 5 ; 
-		var leftOffset = Math.floor(rangeSize/2); 
-		var rightOffset = $scope.totalPages() - leftOffset ; 
-		//alert($scope.totalPages() ) ; 
-
-		var ret = [] ; 
-		var start = $scope.currentPage ; 
-
-		if (start <= leftOffset) {
-			start = 1 ;
-		} else if (start >= rightOffset) {
-			start = $scope.totalPages() - rangeSize + 1 ; 
-		} else {
-			start = start - leftOffset
-		}			
-		var end = ((start + rangeSize) > $scope.totalPages()) ? $scope.totalPages() : start + rangeSize -1;  
-		for (var i = start ; i <= end  ; i++) { ret.push(i) ; } 
-		return ret ; 
-	} ;	
-
-	$scope.totalPages = function() {
-		return Math.ceil($scope.tours.length / $scope.itemsPerPage) ; 
-	} ;
-
-	$scope.setPage = function(n) {
-		$scope.currentPage = n ; 
-	} ;
-
-	$scope.prevPage = function() {
-		if ($scope.currentPage > 1) {$scope.currentPage--} ;
-	} ; 
-
-	$scope.prevPageDisabled = function() {
-		return $scope.currentPage === 1 ? "disabled" : "" ;
-	} ;
-
-	$scope.nextPage = function() {
-		if ($scope.currentPage < $scope.totalPages()) {$scope.currentPage++ };
-	} ;
-
-	$scope.nextPageDisabled = function(){
-		return $scope.currentPage === $scope.totalPages() ? "disabled" : "" ; 
-	} ;
-
-
-	$scope.typeFilter = {
-		individual: true, 
-		group: true
-	} ; 
-
-	$scope.durationFilter = {
-		halfDay: true,
-		day: true,
-		dayPlus: true,
-		flexible: true
-	} ; 
-
-	$scope.transportationFilter = {
-		walk: true,
-		auto: true,
-		bike: true
-	} ;
-
-	$scope.filterList = function(tour) {
-		if (($scope.typeFilter.individual === false) && (tour.private_tour === true)) return false; 
-		if (($scope.typeFilter.group === false) && (tour.private_tour === false)) return false ; 
-		if (($scope.durationFilter.flexible === false) && (tour.duration_unit === 'flexible')) return false
-		if (($scope.durationFilter.dayPlus === false) && (tour.duration_unit === 'day')) return false ; 
-		if (($scope.durationFilter.day === false) && (tour.duration_unit === 'hour') && (tour.duration_size > 6)) return false ; 
-		if (($scope.durationFilter.halfDay === false) && (tour.duration_unit === 'hour') && (tour.duration_size <= 6)) return false ; 
-		if (($scope.transportationFilter.walk === false) && (tour.tour_type === 'walking')) return false ; 
-		if (($scope.transportationFilter.auto === false) && (tour.tour_type === 'private_car')) return false ; 
-		if (($scope.transportationFilter.bike === false) && (tour.tour_type === 'bicycle')) return false ; 
-		
-		return true;
-	} ; 
-}])  
-	.filter('formatPrice', function(fetchData) {	
-		return function(input) {	
-			var price = input.main_price_amount.toLocaleString()  ;  
-
-			fetchData.currencies(function(data) {	
-				var currenciesJSON = data ;
-				var foundRight = _.find(RIGHT_SIDE_CURRENCIES, function(currency) {
-					return currency === input.main_price_currency_code ; 
-				}) ;
-
-				if (typeof foundRight === 'undefined') {
-					price = currenciesJSON[input.main_price_currency_code].symbol + ' ' + price ;
-				} else {
-					price += " " + currenciesJSON[input.main_price_currency_code].symbol ; 
-				}
-				return price ; 
-			}) ;			
-		}
-	})
+		$scope.filterList = function(tour) {
+			if (($scope.typeFilter.individual === false) && (tour.private_tour === true)) return false; 
+			if (($scope.typeFilter.group === false) && (tour.private_tour === false)) return false ; 
+			if (($scope.durationFilter.flexible === false) && (tour.duration_unit === 'flexible')) return false
+			if (($scope.durationFilter.dayPlus === false) && (tour.duration_unit === 'day')) return false ; 
+			if (($scope.durationFilter.day === false) && (tour.duration_unit === 'hour') && (tour.duration_size > 6)) return false ; 
+			if (($scope.durationFilter.halfDay === false) && (tour.duration_unit === 'hour') && (tour.duration_size <= 6)) return false ; 
+			if (($scope.transportationFilter.walk === false) && (tour.tour_type === 'walking')) return false ; 
+			if (($scope.transportationFilter.auto === false) && (tour.tour_type === 'private_car')) return false ; 
+			if (($scope.transportationFilter.bike === false) && (tour.tour_type === 'bicycle')) return false ; 
+			
+			return true;
+		} ; 
+	}])  
 	.filter('formatTourType', function() {
 		return function(input) {
 			if (input) return "개별 여행" ; 
--- assets/javascripts/tourFactories.js
+++ assets/javascripts/tourFactories.js
@@ -7,6 +7,9 @@ var currenciesUrl = 'https://s3-ap-northeast-1.amazonaws.com/mrt-testdata/jsons/
 	offers.json 의 "main_price_amount" --> 이미 currencies.json "rate" 적용된 가격 표시?.  
 */
 
+var LEFT_SIDE_CURRENCIES = ["KRW", "USD", "EUR", "JPY", "TWD", "THB", "NZD", "MYR", "HKD", "GBP", "CNY", "CAD", "AUD" ] ; 
+var RIGHT_SIDE_CURRENCIES = ["CHF", "RUB", "DKK"] ; 
+
 angular.module('tourFactories', [])
 	.factory('fetchData', ['$http', function($http) {
 		return {
@@ -16,11 +19,12 @@ angular.module('tourFactories', [])
 			guides: $http({method: 'GET', url: guidesUrl, cache: true})
 		} ; 
 	}])
-	.factory('processData', ['$q', 'fetchData', function($q, fetchData) {
+	.factory('processTourData', ['$q', 'fetchData', function($q, fetchData) {
 		var promises = {
 			offers : fetchData.offers,
 			guides: fetchData.guides,
-			countries: fetchData.countries
+			countries: fetchData.countries, 
+			currencies: fetchData.currencies
 		} ; 
 		
 		return function(callback) {
@@ -29,6 +33,7 @@ angular.module('tourFactories', [])
 				var cities_min = {} ; 
 				var tours = res.offers.data ; 
 				var guides = res.guides.data ;
+				var currency = res.currencies.data ;
 
 				_.each(cities, function iterateCities(element, index, list) {
 					cities_min[element.id] = element.locale_names.ko ; 
@@ -41,6 +46,19 @@ angular.module('tourFactories', [])
 					tours[index].username = ourGuide.user.username ; 
 					tours[index].profile_medium_url = ourGuide.user.profile_medium_url ;  
 					tours[index].cityName = cities_min[element.city_info_id] ; 
+
+					// format price
+					var price = tours[index].main_price_amount.toLocaleString()  ;  
+					var foundRight = _.find(RIGHT_SIDE_CURRENCIES, function(currency) {
+						return currency === tours[index].main_price_currency_code ; 
+					}) ;
+					
+					if (typeof foundRight === 'undefined') {
+						price = currency[tours[index].main_price_currency_code].symbol + ' ' + price ;
+					} else {
+						price += " " + currency[tours[index].main_price_currency_code].symbol ; 
+					}
+					tours[index].price = price ; 
 				});	
 				callback(tours) ; 
 			}) ; 		
